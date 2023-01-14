60d27d327cd4586da4bcd31863a637fc58f25a96
Kamogelo Mpoko <112622646+KamogeloMpoko@users.noreply.github.com>
2023-01-14 13:30:37 -0800

    Add files via upload

---
+++ Final project/faq.js
@@ -0,0 +1,13 @@
+var acc = document.getElementsByClassName("collapsible");
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
